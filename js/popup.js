06364e5a46534cb944996fce7c35484603eba6ac
lukeon <lukeon.hajek@gmail.com>
2026-01-02 16:38:55 +0100

    new pop-up

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const CURRENT_VERSION = "v2.2"; // version
+
     const popupContainer = document.getElementById('popup-container');
     const closeBtn = document.getElementById('close-btn');
     const popupOverlay = document.getElementById('popup-overlay');
-    const popupWasShown = localStorage.getItem('popupWasShown');
 
-    if (!popupWasShown) {
+    const seenVersion = localStorage.getItem('popupSeenVersion');
+
+    if (seenVersion !== CURRENT_VERSION) {
         popupContainer.classList.remove('popup-hidden');
     }
 
     function closePopup() {
         popupContainer.classList.add('popup-hidden');
-        localStorage.setItem('popupWasShown', 'true');
+        localStorage.setItem('popupSeenVersion', CURRENT_VERSION);
     }
 
     closeBtn.addEventListener('click', closePopup);
     popupOverlay.addEventListener('click', closePopup);
-
 });
